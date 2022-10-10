d228025883247dd34bb656407ea6951c36e47cb0
byronEsson <byronesson767@gmail.com>
2022-10-10 14:42:10 +0100

    correct condition in error handler

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  if (err) {
+  if (err.status) {
     res.status(err.status).send({ msg: err.msg });
   } else next(err);
 });
