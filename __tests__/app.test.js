32003829125e244428dc6cb235872b4075936a9c
byronEsson <byronesson767@gmail.com>
2022-10-10 11:16:10 +0100

    create testsuite

---
+++ __tests__/app.test.js
@@ -0,0 +1,12 @@
+const app = require("../app");
+const request = require("supertest");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data");
+
+beforeEach(() => {
+  return seed(data);
+});
+afterAll(() => {
+  db.end();
+});
