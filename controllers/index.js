91ab95ecaf4687aaab1e7cdd0badff154a006edd
byronEsson <byronesson767@gmail.com>
2022-10-12 12:10:01 +0100

    add /get/api endpoint

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,15 @@ const categories = require("./categories-controller");
 const reviews = require("./reviews-controller.js");
 const comments = require("./comments-controller");
 const users = require("./users-controller");
+const { readEndpoints } = require("../models");
 
 Object.assign(exports, categories);
 Object.assign(exports, reviews);
 Object.assign(exports, comments);
 Object.assign(exports, users);
 
-
+exports.getEndpoints = (req, res, next) => {
+  readEndpoints().then((endpoints) => {
+    res.status(200).send({ endpoints });
+  });
+};
--- models/index.js
+++ models/index.js
@@ -2,8 +2,17 @@ const categories = require("./categories-model");
 const reviews = require("./reviews-model");
 const comments = require("./comments-model");
 const users = require("./users-model");
+const fs = require("fs/promises");
 
 Object.assign(exports, categories);
 Object.assign(exports, reviews);
 Object.assign(exports, comments);
 Object.assign(exports, users);
+
+exports.readEndpoints = () => {
+  return fs
+    .readFile(__dirname + "/../endpoints.json", "utf-8")
+    .then((endpoints) => {
+      return JSON.parse(endpoints);
+    });
+};
