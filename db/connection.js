9ed409effaf4200f0ba49d0ea2f7bbdb8c905a78
byronEsson <byronesson767@gmail.com>
2022-10-13 10:55:44 +0100

    revert changes to seed:prod script

--- db/connection.js
+++ db/connection.js
@@ -5,6 +5,9 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE not set");
+}
 const config =
   ENV === "production"
     ? {
@@ -14,9 +17,5 @@ const config =
         },
       }
     : {};
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE not set");
-}
 
 module.exports = new Pool(config);
